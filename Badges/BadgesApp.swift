886d3d3366e0be8ecc0680c7fb6e0f4b20dffee1
Klevia Ulqinaku <klevia.96@gmail.com>
2022-12-01 16:20:55 +0100

    Initial Commit

---
+++ Badges/BadgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgesApp.swift
+//  Badges
+//
+//  Created by Klevia Ulqinaku on 01.12.22.
+//
+
+import SwiftUI
+
+@main
+struct BadgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Badges/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Badges
+//
+//  Created by Klevia Ulqinaku on 01.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
