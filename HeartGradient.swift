58da551d5a38a2d4597f18d9b789f63cf23c6cbf
Rishabh Maini <rishabh.maini@student.kit.edu>
2022-12-04 20:50:51 +0530

    Heart example included

--- HeartGradient.swift
+++ HeartGradient.swift
@@ -14,7 +14,36 @@ struct HeartGradient: View {
             
             Color.black.ignoresSafeArea()
             
+            
+            
+            
             Image("Heart")
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .opacity(0)
+                
+                .background(
+                
+                    LinearGradient(gradient: Gradient(stops: [
+                            Gradient.Stop(color: Color(hex: "0A0A0A"), location: 0),
+                            Gradient.Stop(color: Color(hex: "FFFFFF"), location: 0.5),
+                            Gradient.Stop(color: Color(hex: "FFFAAA"), location: 1)
+                        ]), startPoint: .leading, endPoint: .trailing)
+                    .mask(
+                        
+                        Image("Heart")
+                            .resizable()
+                            .aspectRatio( contentMode: .fit)
+                        
+                    )
+                
+                
+                )
+                
+                
+            
+                
+                
             
             
         }
