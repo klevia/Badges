76f33d403256d87a6a657248e22b003f80f025cd
Rishabh Maini <rishabh.maini@student.kit.edu>
2022-12-04 20:35:23 +0530

    Color - hex fixed. Colors added to database.

---
+++ HeartGradient.swift
@@ -0,0 +1,28 @@
+//
+//  HeartGradient.swift
+//  Badges
+//
+//  Created by Rishabh Maini on 04/12/22.
+//
+
+import SwiftUI
+
+struct HeartGradient: View {
+    var body: some View {
+        
+        ZStack{
+            
+            Color.black.ignoresSafeArea()
+            
+            Image("Heart")
+            
+            
+        }
+    }
+}
+
+struct HeartGradient_Previews: PreviewProvider {
+    static var previews: some View {
+        HeartGradient()
+    }
+}
